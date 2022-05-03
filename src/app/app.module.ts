ff6a95e9eb9fefc5714af8769b30289bb3ea12af
YuliyaIv <ranokgps@gmail.com>
2022-05-03 15:20:25 +0200

    created components, created interface Userr and Courrses

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SectionComponent } from './components/section/section.component';
+import { CoursesComponent } from './components/courses/courses.component';
+import { CoursesListComponent } from './components/courses/courses-list/courses-list.component';
+import { LogoComponent } from './components/logo/logo.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { BreadcrumbsComponent } from './components/breadcrumbs/breadcrumbs.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SectionComponent,
+    CoursesComponent,
+    CoursesListComponent,
+    LogoComponent,
+    FooterComponent,
+    BreadcrumbsComponent,
   ],
   imports: [
     BrowserModule
